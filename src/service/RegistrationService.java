8b397bdc07a6c91b0b965c3db2f025dbd9a31537
Phyoe Kyaw Zin <phyoekyawzin10@gmail.com>
2026-04-29 18:05:05 +0700

    I have not add anything yet. Sorry

---
+++ src/service/RegistrationService.java
@@ -0,0 +1,12 @@
+package service;
+
+import model.Registration;
+
+public class RegistrationService {
+
+   
+    public String registerUserToEvent(Registration newReg) {
+       
+        return "Success: Registered!";
+    }
+}
